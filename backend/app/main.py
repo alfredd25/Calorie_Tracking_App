7d92d1d9ae1537c12c2b823b62ee4e0302c6f805
alfredd25 <alfredbiju7@gmail.com>
2026-03-22 11:22:13 +0530

    feat: Add Prometheus and Grafana monitoring with custom dashboard

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth, food, meal
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost",
-    ],
+    allow_origins=["http://localhost:3000", "http://localhost"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -18,3 +16,5 @@ app.add_middleware(
 app.include_router(auth.router, prefix="/auth")
 app.include_router(food.router)
 app.include_router(meal.router)
+
+Instrumentator().instrument(app).expose(app)
